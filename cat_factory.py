b377ecd81328fada75ff062d02234093d512000a
Elvin Ramirez <real8686@gmail.com>
2018-07-21 10:02:39 -0400

    func for full path

--- cat_factory.py
+++ cat_factory.py
@@ -1,11 +1,14 @@
 import os
+import cat_services
 
 def main():
     # print the header
     print_header()
     # get or create output folder
-    get_or_create_output_folder()
+    folder = get_or_create_output_folder()
+    print('Found or Created folder: ' + folder)
     # download cats
+    download_cats(folder)
     # display cats
 
 def print_header():
@@ -15,10 +18,19 @@ def print_header():
 
 
 def get_or_create_output_folder():
-    print(__file__)
+    #print(__file__)
     folder = 'cat_pictures'
     full_path = os.path.abspath(os.path.join('.',folder))
-    print(full_path)
+    #print(full_path)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print('Creating new directory at {}'.format(full_path))
+        os.mkdir(full_path)
+
+def download_cats(folder):
+   pass 
+
+
 
 
 if __name__ == '__main__':
---
+++ cat_services.py
@@ -0,0 +1,4 @@
+
+
+def get_cats(folder, name):
+    pass
